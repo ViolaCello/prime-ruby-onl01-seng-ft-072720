1d1578d91ec5259b13260ef8ed6585e023635eed
Anthony Lepore <violacellonyc@gmail.com>
2020-07-30 17:55:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
 # Add  code here!
+
+require 'pry'
+
+def prime?(num)
+  if num > 1 && num < 4
+    return true
+  elsif num < 2 == true then return false 
+  elsif num.even? == true then return false 
+
+  else
+    
+a = (2 .. (num - 1)).to_a 
+
+a.collect do |i|
+ if (num % i) == 0 then return false
+
+ 
+    
+   end 
+  end
+
+  end
+   true
+end
