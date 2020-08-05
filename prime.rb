74e2d3d8455d165f43e19e90834980df7806578f
Anthony Lepore <violacellonyc@gmail.com>
2020-08-05 18:47:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -13,7 +13,11 @@ def prime?(num)
 a = (2 .. (num - 1)).to_a 
 
 a.collect do |i|
+<<<<<<< HEAD
  if (num % i) == 0 then return false
+=======
+ if (num % i) == 0 then false
+>>>>>>> bb9ce1ec27bff34c2a0771e949c24d89d2183bf9
 
  
     
